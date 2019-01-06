454fe04171804262fb2c0cd33259f1f7340893f2
Mingxiang Xue <mingxiangxue@gmail.com>
2019-01-06 17:57:33 +0800

    Add cors test utility

--- bin/cp_with_intermediate_dir.sh
+++ bin/cp_with_intermediate_dir.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
+#
+# The purpose of this script is to keep the intermediate path when copying files.
+# The -r/-R option of `cp` is not supported, so only file is supported.
+#
+# Examples:
+#
+# * Copy two files to /tmp:
+#
+#     ./cp_with_intermediate_dir.sh foo/bar.txt foo/bar/baz.txt /tmp
+#
+#     The tree of /tmp will be:
+#       - /tmp
+#            | foo
+#                | bar.txt
+#                | bar
+#                    | baz.txt
+#
+# * Copy files whose paths are specified by using pipe:
+#
+#     git status | grep 'modified' | awk '{ print $2  }' | xargs -J % cp_with_intermediate_dir.sh % /tmp
 
 dest_dir=${@: -1}
 for file in ${@:1:`expr $# - 1`}; do
   dest_file="$dest_dir/$file"
   mkdir -p `dirname $dest_file`
+  echo "Copy $file to $dest_file"
   cp "$file" "$dest_file"
 done
