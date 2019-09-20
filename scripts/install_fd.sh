da8c748d591dabe169afac292f07657bbb9cbaea
Mingxiang Xue <mingxiangxue@gmail.com>
2019-09-20 23:17:11 +0800

    Add other linux system support

--- scripts/install_fd.sh
+++ scripts/install_fd.sh
@@ -4,22 +4,26 @@
 
 set -e
 
+abort() {
+  echo $@ >>/dev/stderr
+  exit 1
+}
+
 case $OSTYPE in
   darwin*)
-    if ! type brew >/dev/null; then
-      echo 'You must install `brew` before you can continue'
-      exit 1
+    if ! type brew &>/dev/null; then
+      abort 'You must install `brew` before you can continue'
     fi
     brew install fd
     ;;
   linux-gnu)
-    if ! type apt-get >/dev/null; then
-      echo 'Only debian system supported'
-      exit 1
+    if type dpkg &>/dev/null; then
+      dest=/tmp/fd_7.3.0_amd64.deb
+      wget 'https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb' -O $dest
+      sudo dpkg -i $dest
+      rm $dest
+    else
+      npm install -g fd-find
     fi
-    dest=/tmp/fd_7.3.0_amd64.deb
-    wget 'https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb' -O $dest
-    sudo dpkg -i $dest
-    rm $dest
     ;;
 esac
--- scripts/install_fzf.sh
+++ scripts/install_fzf.sh
@@ -4,20 +4,25 @@
 
 set -e
 
+abort() {
+  echo $@ >>/dev/stderr
+  exit 1
+}
+
 case $OSTYPE in
   darwin*)
-    if ! type brew >/dev/null; then
-      echo 'You must install `brew` before you can continue'
-      exit 1
+    if ! type brew &>/dev/null; then
+      abort 'You must install `brew` before you can continue'
     fi
     brew install fzf
     /usr/local/opt/fzf/install --key-bindings --completion --no-bash --no-update-rc
     ;;
   linux-gnu)
-    if ! type apt-get >/dev/null; then
-      echo 'Only debian system supported'
-      exit 1
+    if type apt-get &>/dev/null; then
+      sudo apt-get install fzf
+    else
+      git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+      sudo ~/.fzf/install
     fi
-    sudo apt-get install fzf
     ;;
 esac
