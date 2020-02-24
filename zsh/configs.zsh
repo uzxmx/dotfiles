2665b422b5fa050c238b1179f918e728309a4e2d
Mingxiang Xue <mingxiangxue@gmail.com>
2020-02-24 10:58:47 +0800

    Automatically source ~/.zshrc when running rcup for the first time
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- zsh/configs.zsh
+++ zsh/configs.zsh
@@ -94,8 +94,10 @@ fi
 # Bash like completion
 setopt noautomenu
 
-# So as not to be disturbed by Ctrl-S ctrl-Q in terminals
-stty -ixon
+if [ -t 0 ]; then
+  # So as not to be disturbed by Ctrl-S ctrl-Q in terminals
+  stty -ixon
+fi
 
 # Case-insensitive matching only if there are no case-sensitive matches
 # Ref: https://superuser.com/questions/1092033/how-can-i-make-zsh-tab-completion-fix-capitalization-errors-for-directorys-and
