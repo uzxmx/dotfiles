a8aaed1ed69a5df35cadcb26e182162f5ccd1497
Mingxiang Xue <mingxiangxue@gmail.com>
2020-05-21 16:35:29 +0800

    Update profiles.json to settings.json for new verion of WindowsTerminal
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

---
+++ scripts/sync_windows_terminal_settings.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Synchronize ~/.dotfiles/windows_terminal/settings.json with C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+
+set -eo pipefail
+
+. $(dirname "$0")/utils.sh
+
+USE_DEV_BUILD=${USE_DEV_BUILD:-1}
+
+username="$(cmd.exe /c 'echo %USERNAME%' | sed 's/[[:space:]]$//')"
+if [ "$USE_DEV_BUILD" = "1" ]; then
+  dir="/mnt/c/Users/$username/AppData/Local/Packages/WindowsTerminalDev_8wekyb3d8bbwe/LocalState"
+else
+  dir="/mnt/c/Users/$username/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"
+fi
+if [ ! -d "$dir" ]; then
+  abort "Directory '$dir' doesn't exist."
+fi
+
+file="$dir/settings.json"
+cat >"$file" <<EOF
+// Automatically generated from ~/.dotfiles/windows_terminal/settings.json. DO NOT EDIT.
+//
+EOF
+cat ~/.dotfiles/windows_terminal/settings.json >>"$file"
+
+echo 'Successful.'
--- vim/ftplugin/json.vim
+++ vim/ftplugin/json.vim
@@ -6,17 +6,17 @@ function! s:on_event(job_id, data, event)
   endif
 endfunction
 
-function! s:sync_windows_terminal_profiles()
+function! s:sync_windows_terminal_settings()
   let callbacks = {
   \ 'on_stdout': function('s:on_event'),
   \ 'on_stderr': function('s:on_event'),
   \ 'on_exit': function('s:on_event')
   \ }
-  let jid = jobstart('~/.dotfiles/scripts/sync_windows_terminal_profiles.sh', callbacks)
+  let jid = jobstart('~/.dotfiles/scripts/sync_windows_terminal_settings.sh', callbacks)
   call jobwait([jid])
 endfunction
 
-augroup windows_terminal_profiles
+augroup windows_terminal_settings
   au!
-  autocmd BufWritePost ~/.dotfiles/windows_terminal/profiles.json call s:sync_windows_terminal_profiles()
+  autocmd BufWritePost ~/.dotfiles/windows_terminal/settings.json call s:sync_windows_terminal_settings()
 augroup END
