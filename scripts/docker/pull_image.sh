538abf38e798860bd7efa5d2ed94b744655ffc56
Mingxiang Xue <mingxiangxue@gmail.com>
2021-01-12 23:50:38 +0800

    Add utility to pull docker images from a mirror
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

---
+++ scripts/docker/pull_image.sh
@@ -0,0 +1,28 @@
+usage_p() {
+  cat <<-EOF 1>&2
+Usage: docker p <image>
+
+Pull an image from a mirror.
+EOF
+  exit 1
+}
+
+cmd_p() {
+  local name="$1"
+  if [ -z "$name" ]; then
+    echo 'An image name is required.'
+    exit 1
+  fi
+
+  local fullname
+  if [[ ! "$name" =~ .+/.+ ]]; then
+    fullname="library/$name"
+  else
+    fullname="$name"
+  fi
+
+  local mirror="docker.mirrors.ustc.edu.cn"
+  echo "Pulling image from $mirror..."
+  docker pull "$mirror/$fullname"
+  docker tag "$mirror/$fullname" "$name"
+}
