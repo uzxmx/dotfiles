a09125e37e5d48fed5ece58614fb727a5e99dbc3
Mingxiang Xue <mingxiangxue@gmail.com>
2021-01-24 20:46:30 +0800

    Able to change to a parent directory quickly
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

---
+++ scripts/cd/parent.sh
@@ -0,0 +1,20 @@
+# There is no need for this command to support changing to directories like `/` or `/foo`,
+# because it's NOT that annoying to use the original 'cd' for those cases.
+dir="$(dirname $(pwd))"
+while [ ! "$dir" = "/" ]; do
+  if [ -z "$directories" ]; then
+    directories="$dir"
+  else
+    directories="$dir
+$directories"
+  fi
+  dir="$(dirname "$dir")"
+done
+
+directories="$(echo "$directories" | sed 1d)"
+if [ -z "$directories" ]; then
+  echo "You'd better to use this command when the current working directory is too deep."
+  exit
+fi
+
+main_loop_with_fzf --prompt "(CTRL-Y:yank CTRL-E:edit) Select a parent directory> " --expect="ctrl-e"
