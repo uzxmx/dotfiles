48402b1e7607b36be64d495708f7edc3da9991b8
Mingxiang Xue <mingxiangxue@gmail.com>
2021-02-17 23:37:22 +0800

    Able to find file hierarchical with multiple candidates
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/lib/utils/find.sh
+++ scripts/lib/utils/find.sh
@@ -11,19 +11,37 @@
 #   find_file_hierarchical foo
 #   find_file_hierarchical foo /tmp/bar/some-directory
 find_file_hierarchical() {
-  local name="$1"
-  local dir="$(realpath "${2:-.}")"
+  find_file_hierarchical_with_candidates . "$1"
+}
 
-  while true; do
-    if [ -e "$dir/$name" ]; then
-      echo "$dir/$name"
-      break
-    fi
+# Find file hierarchically that the current directory is first checked, if not
+# found, it goes up to parent directory and check, until root is reached. If
+# the first candidate is not found, then the second is tried, and so on.
+#
+# @params:
+#   $1: the starting directory
+#   VARARGS: candidate files to find
+#
+# @example
+#   find_file_hierarchical_with_candidates /starting-directory candidate1 candidate2
+find_file_hierarchical_with_candidates() {
+  local old_pwd="$(realpath "$1")"
+  shift
 
-    if [ "$dir" = "/" ]; then
-      break
-    fi
+  local name dir
+  for name in "$@"; do
+    dir="$old_pwd"
+    while true; do
+      if [ -e "$dir/$name" ]; then
+        echo "$dir/$name"
+        return
+      fi
 
-    dir="$(dirname $dir)"
+      if [ "$dir" = "/" ]; then
+        break
+      fi
+
+      dir="$(dirname $dir)"
+    done
   done
 }
