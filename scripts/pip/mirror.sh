40a29b2328769afc622e0b94f9997d99eda8d1b2
Mingxiang Xue <mingxiangxue@gmail.com>
2021-03-31 20:03:22 +0800

    Add mirror subcommand for pip
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

---
+++ scripts/pip/mirror.sh
@@ -0,0 +1,38 @@
+usage_mirror() {
+  cat <<-EOF
+Usage: pip mirror [-e | -d]
+
+Manage(show/enable/disable) pip mirror. By default it shows whether mirror is
+enabled.
+
+Options:
+  -e Enable mirror
+  -d Disable mirror
+EOF
+  exit 1
+}
+
+cmd_mirror() {
+  local action="show"
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+      -e)
+        action="enable"
+        ;;
+      -d)
+        action="disable"
+        ;;
+      *)
+        usage_mirror
+        ;;
+    esac
+    shift
+  done
+
+  if [ "$action" = "enable" ]; then
+    pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+  elif [ "$action" = "disable" ]; then
+    pip config unset global.index-url
+  fi
+  pip config list | grep global.index-url=
+}
