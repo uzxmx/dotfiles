7ee0a00a497690a9f6302177264de4886ab9c081
Mingxiang Xue <mingxiangxue@gmail.com>
2021-06-15 17:30:12 +0800

    Update docker logs subcommand

--- scripts/docker/logs.sh
+++ scripts/docker/logs.sh
@@ -4,18 +4,19 @@ usage_logs() {
   cat <<-EOF
 Usage: docker logs
 
-Show logs of a container selected by fzf.
+Show logs of a container selected by fzf. Extra arguments will be passed to the
+original intact.
 EOF
   exit 1
 }
 
 cmd_logs() {
+  local id="$(select_container)"
+  [ -z "$id" ] && exit
   if [ "$#" -gt 0 ]; then
-    docker logs "$@"
+    docker logs "$id" "$@"
   else
-    local id="$(select_container)"
-    [ -z "$id" ] && exit
-    docker logs -f "$id"
+    docker logs "$id" -f -n 10
   fi
 }
 alias_cmd l logs
