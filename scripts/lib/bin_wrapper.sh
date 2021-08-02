5c9f95e33abd78f04e458ebe1b030fe0ef624a45
Mingxiang Xue <mingxiangxue@gmail.com>
2021-08-02 14:43:13 +0800

    Update bin_wrapper utility to avoid changing PATH env
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/lib/bin_wrapper.sh
+++ scripts/lib/bin_wrapper.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-source $(dirname "$BASH_SOURCE")/path.sh
-PATH="$(new_path_exclude "$dotfiles_dir/bin")"
+_origin_name="$(basename "$0")"
+
+update_origin_path() {
+  _origin_path="$(source "$dotfiles_dir/scripts/lib/path.sh"; PATH="$(new_path_exclude "$dotfiles_dir/bin")"; which $_origin_name || true)"
+}
+
+update_origin_path
+
+run_origin() {
+  [ -z "$_origin_path" ] && echo "Cannot find the wrapped origin." >&2 && exit 1
+  "$_origin_path" "$@"
+}
 
 ensure_origin_exists() {
-  local cmd="${1:-$(basename "$0")}"
-  if ! type -p "$cmd" &>/dev/null; then
-    echo "Oops, command '$cmd' not found." >&2
+  _origin_name="${1:-$_origin_name}"
+  if ! type -p "$_origin_name" &>/dev/null; then
+    echo "Oops, command '$_origin_name' not found." >&2
 
     echo -n "Would you like to install it? (Y/n)" >&2
     local input
@@ -16,13 +26,12 @@ ensure_origin_exists() {
       exit
     fi
 
-    local install_script="${2:-$cmd}"
+    local install_script="${2:-$_origin_name}"
     if [[ ! "$install_script" =~ ^/ ]]; then
       install_script="$dotfiles_dir/scripts/install/$install_script"
     fi
 
     "$install_script"
   fi
-
-  true
+  update_origin_path
 }
