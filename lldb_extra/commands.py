e13eca1f19534a5c228b4a02e9c365d6ef2293b9
Mingxiang Xue <mingxiangxue@gmail.com>
2021-08-05 14:03:25 +0800

    Enhance lldb by adding custom command and doing custom initialization
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

---
+++ lldb_extra/commands.py
@@ -0,0 +1,62 @@
+import os
+import lldb
+import optparse
+import shlex
+import tempfile
+import re
+
+def dump_function(debugger, command, result, internal_dict):
+    usage = '''usage: %prog <address> [-d <directory>] [-f <file>]
+
+When no file is specified, the file will be named by the function name.
+When no directory is specified, current working directory will be used.'''
+    parser = optparse.OptionParser(prog='dump_function', usage=usage)
+    parser.add_option('-d', '--dir', dest='dir', help='directory to store the generated file')
+    parser.add_option('-f', '--file', dest='file', help='path to the generated file, can be used w/o `-d` option')
+
+    try:
+        (options, args) = parser.parse_args(shlex.split(command))
+    except:
+        return
+
+    if len(args) == 0:
+        print('You need to specify an address which is contained by the function.')
+        return
+
+    file = None
+    file_is_tmp = True
+    dest_path = None
+    if options.file is not None:
+        dest_path = os.path.join(options.dir if options.dir is not None else os.getcwd(), options.file)
+        file = open(dest_path, 'w')
+        file_is_tmp = False
+    else:
+        file = tempfile.TemporaryFile(mode='w+')
+
+    new_output = lldb.SBFile.Create(file)
+    saved_output = debugger.GetOutputFile()
+    saved_use_color = debugger.GetUseColor()
+    debugger.SetUseColor(False)
+    debugger.SetOutputFile(new_output)
+    debugger.HandleCommand('disassemble -a "%s" --force' % args[0])
+    debugger.SetOutputFile(saved_output)
+    debugger.SetUseColor(saved_use_color)
+
+    if file_is_tmp:
+        file.seek(0)
+        name = str(file.readline().strip())
+        name = re.sub(r'^[^0-9a-zA-Z]*(.*?)[^0-9a-zA-Z]*$', r'\1', name)
+        name = re.sub(r'[^0-9a-zA-Z]', '_', name)
+        dest_path = os.path.join(options.dir if options.dir is not None else os.getcwd(), name + '.asm')
+        target_file = open(dest_path, 'w')
+        file.seek(0)
+        target_file.writelines(file.readlines())
+        target_file.close()
+
+    new_output.Close()
+
+    print('Function has been dumped to %s.' % dest_path)
+
+def __lldb_init_module(debugger, internal_dict):
+    debugger.HandleCommand('command script add -f commands.dump_function dump_function')
+    print('The "dump_function" command has been installed.')
--- scripts/lldb/start.sh
+++ scripts/lldb/start.sh
@@ -5,6 +5,13 @@ Usage: lldb start <executable>
 Start lldb and stop at the entrypoint.
 By default, the stdin/stdout/stderr of the program is redirected to '/dev/null'.
 
+This script also imports 'lldb_extra' directory in this dotfiles repo to
+PYTHONPATH, so you can use anything provided by 'lldb_extra' when you're in the
+python interpreter.
+
+You can put lldb commands in a file named as 'lldbinit.post' in current working
+directory, it will be loaded when the program stops at the entrypoint.
+
 Options:
   -i <file> where the stdin is redirected
   -o <file> where the stdout is redirected
@@ -45,8 +52,16 @@ cmd_start() {
     exit 1
   fi
 
+  local lldbinit_post
+  if [ -f lldbinit.post ]; then
+    lldbinit_post="$(cat lldbinit.post)"
+  fi
+
   local lldb_commands="$(cat <<EOF
+command script import $dotfiles_dir/lldb_extra
+command script import $dotfiles_dir/lldb_extra/commands.py
 process launch --stop-at-entry -i "${stdin:-/dev/null}" -o "${stdout:-/dev/null}" -e "${stderr:-/dev/null}"
+$lldbinit_post
 dis -p
 EOF
 )"
