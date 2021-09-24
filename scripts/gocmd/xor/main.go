a16ec8ef9cc33f912066a09c8de30d10c94de173
Mingxiang Xue <mingxiangxue@gmail.com>
2021-09-24 21:40:08 +0800

    Improve xor cmd
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/gocmd/xor/main.go
+++ scripts/gocmd/xor/main.go
@@ -2,47 +2,79 @@ package main
 
 import (
 	"encoding/hex"
-	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func main() {
-	flag.Usage = usage
-	flag.Parse()
+func usage() {
+	fmt.Fprintf(os.Stderr, `Usage: xor <operand1-in-hex> <operand2-in-hex>
 
-	args := flag.Args()
+Compute the exclusive or of the two operands. The operand should be specified
+in hexadecimal.
+
+Options:
+  -r Pad with zeros right when the lenght of operands is not equal (default is left)
+
+Examples:
+  xor ff00 00ff
+  xor ff00 "00  ff "
+
+  xor ff00 ff     // => ffff
+  xor ff00 ff -r  // => 0000
+`)
+	os.Exit(1)
+}
+
+func main() {
+	var args []string
+	padRight := false
+
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+		switch arg {
+		case "-r":
+			padRight = true
+		default:
+			if len(arg) > 0 && arg[0] == '-' {
+				usage()
+			}
+			args = append(args, arg)
+		}
+	}
+
 	if len(args) != 2 {
 		usage()
 		os.Exit(1)
 	}
 
-	a, err := hex.DecodeString(args[0])
+	a, err := hex.DecodeString(strings.ReplaceAll(args[0], " ", ""))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	b, err := hex.DecodeString(args[1])
+	b, err := hex.DecodeString(strings.ReplaceAll(args[1], " ", ""))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	var d []byte
-	if len(a) > len(b) {
-		d = xor(a, a, b, len(b))
-	} else {
-		d = xor(b, a, b, len(a))
+
+	if len(a) != len(b) {
+		if len(a) < len(b) {
+			c := a
+			a = b
+			b = c
+		}
+		// Here len(b) < len(a). So we need to pad b with zeros to make sure they are equal.
+		c := make([]byte, len(a))
+		if padRight {
+			copy(c, b)
+		} else {
+			copy(c[len(a)-len(b):], b)
+		}
+		b = c
 	}
-	fmt.Println(hex.EncodeToString(d))
-}
-
-func usage() {
-	fmt.Fprintf(os.Stderr, `Usage: xor <operand1> <operand2>
-
-Compute the exclusive or of the two operands. The operand should be specified
-in hexadecimal.
-`)
-	flag.PrintDefaults()
+	fmt.Println(hex.EncodeToString(xor(a, a, b, len(a))))
 }
 
 func xor(dst, a, b []byte, n int) []byte {
