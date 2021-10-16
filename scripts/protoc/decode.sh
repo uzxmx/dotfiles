1ba0cd6b9943f2618a0c2b40b2bb676919695e1c
Mingxiang Xue <mingxiangxue@gmail.com>
2021-10-16 14:51:00 +0800

    Able to decode data as a proto message
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/protoc/decode.sh
+++ scripts/protoc/decode.sh
@@ -1,18 +1,63 @@
 usage_decode() {
   cat <<-EOF
-Usage: protoc decode <file>
+Usage: protoc decode <data-file>
 
-Decode protocol message.
+Decode protocol message. The data file can be either a binary file, or a file
+containing hexadecimal text.
+
+Optionally, you can specify a message file and a message type to decode the
+data to a message.
+
+Options:
+  -p <proto_file> proto message definition file
+  -t <message_type> proto message type
 EOF
   exit 1
 }
 
 cmd_decode() {
-  source "$dotfiles_dir/scripts/lib/tmpfile.sh"
+  local proto_file message_type data_file
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+      -p)
+        shift
+        proto_file="$1"
+        ;;
+      -t)
+        shift
+        message_type="$1"
+        ;;
+      -*)
+        usage_decode
+        ;;
+      *)
+        data_file="$1"
+        ;;
+    esac
+    shift
+  done
 
-  local tmpfile
-  create_tmpfile tmpfile
-  xxd -r -p "$1" "$tmpfile"
-  protoc --decode_raw <"$tmpfile"
+  # The data file seems not a binary file, we try to convert it to binary file first.
+  if [ ! "$(file -b "$data_file")" = "data" ]; then
+    source "$dotfiles_dir/scripts/lib/tmpfile.sh"
+    local tmpfile
+    create_tmpfile tmpfile
+    xxd -r -p "$data_file" "$tmpfile"
+    data_file="$tmpfile"
+  fi
+
+  if [ -n "$proto_file" ]; then
+    if [ -z "$message_type" ]; then
+      message_type="$(basename "$proto_file" | sed 's/\..*$//')"
+      echo -n "Infer message type from proto file: " >&2
+      echo "$message_type"
+    fi
+    local include_dir="$(dirname "$proto_file")"
+    echo "Infer proto include path: $include_dir" >&2
+
+    cat "$data_file" | protoc -I "$include_dir" "$proto_file" --decode="$message_type"
+  else
+    protoc --decode_raw <"$data_file"
+  fi
 }
 alias_cmd d decode
