be6ad5fc64e961e4d44d72f899153a533d77f4a1
Mingxiang Xue <mingxiangxue@gmail.com>
2021-10-24 18:44:17 +0800

    Dump each module into one file, and add support to dump specific modules
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- lldb_extra/commands/cmd_dump_module.py
+++ lldb_extra/commands/cmd_dump_module.py
@@ -1,14 +1,12 @@
 import lldb
 import optparse
 import shlex
+import os
 
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand('command script add -f cmd_dump_module.dump_module dump_module')
     print('The "dump_module" command has been installed.')
 
-# TODO:
-#   1. filter by module
-#   2. better to dump each module into one file with module name as filename
 def dump_module(debugger, command, result, internal_dict):
     '''
     Dump modules.
@@ -22,28 +20,46 @@ def dump_module(debugger, command, result, internal_dict):
         result.SetError(parser.usage)
         return
 
-    if options.file is None:
-        result.SetError('An output file should be specified by `-f` option.')
-        return
+    target = debugger.GetSelectedTarget()
+    if options.all:
+        for m in target.module_iter():
+            do_dump_module(target, m, options.output_dir)
+    elif len(args) > 0:
+        for e in args:
+            module = target.FindModule(lldb.SBFileSpec(e))
+            if not module.IsValid():
+                result.SetError("Unable to find module '{}', to see list of modules, use 'image list -b'".format(e))
+                return
+            do_dump_module(target, module, options.output_dir)
+    else:
+        result.SetError('You should either pass in module names or specify `-a` to dump all modules. (Hint: use `image list -b` to get all modules)')
 
-    with open(options.file, 'w') as f:
-        target = debugger.GetSelectedTarget()
-        for i, m in enumerate(target.module_iter()):
-            if i != 0:
-                f.write('\n========================================\n\n')
-            f.write('Fullpath: %s\n' % m.file.fullpath)
-            f.write('Number of sections: %s\n' % m.num_sections)
-            f.write('Number of symbols: %s\n' % m.num_symbols)
-            if m.num_symbols > 0:
-                f.write('Symbols:\n')
-                for sym in m:
-                    f.write('%s : 0x%x - 0x%x\n' % (sym.name, sym.addr.GetLoadAddress(target), sym.end_addr.GetLoadAddress(target)))
+def do_dump_module(target, m, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    filename = '%s__%s' % (m.file.basename, m.uuid)
+    with open(os.path.join(output_dir, filename), 'w') as f:
+        f.write('Fullpath: %s\n' % m.file.fullpath)
+        f.write('Number of sections: %s\n' % m.num_sections)
+        f.write('Number of symbols: %s\n' % m.num_symbols)
+        if m.num_symbols > 0:
+            f.write('Symbols:\n')
+            for sym in m:
+                f.write('%s : 0x%x - 0x%x\n' % (sym.name, sym.addr.GetLoadAddress(target), sym.end_addr.GetLoadAddress(target)))
 
 def generate_option_parser():
-    usage = "usage: %prog -f <output-file>"
+    usage = "usage: %prog [module-name]..."
     parser = optparse.OptionParser(usage=usage, prog='dump_module')
-    parser.add_option('-f', '--file',
-                      dest='file',
-                      help='File to output')
+    parser.add_option('-a', '--all',
+                      action='store_true',
+                      default=False,
+                      dest='all',
+                      help='Dump all modules, one module per file, with module name and uuid as filename')
+
+    parser.add_option('-d', '--output-dir',
+                      dest='output_dir',
+                      default='modules',
+                      help='The directory to output files, default is `modules`')
 
     return parser
