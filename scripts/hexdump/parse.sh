d49281ae261d0b152070dff3877e27f1905f97bb
Mingxiang Xue <mingxiangxue@gmail.com>
2022-01-01 14:52:33 +0800

    Refactor parse subcommand
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/hexdump/parse.sh
+++ scripts/hexdump/parse.sh
@@ -1,34 +1,68 @@
 usage_parse() {
   cat <<-EOF
-Usage: hexdump parse [in-file]
+Usage: hd parse [in-file]
 
-By default, parse hexadecimal string to binary. Specify '-r' option to do the
-reverse.
+By default, parse hexadecimal string to its binary format. Specify '-r' option
+to convert a file or a string to its hexadecimal string format.
 
-Options:
-  -s <hex-str> Hexadecimal string
-  -r Parse binary to hexadecimal string
+Pipe is supported in both modes.
+
+Common options:
+  -s <str> Input string
+  -r Convert a file or a string to its hexadecimal string format
+
+Options for non-reverse mode:
   --reverse-continuous-bytes-order Reverse continuous bytes order. Continuous
                                    means two bytes not separated by a punctuation or line break.
                                    This is useful when dealing with endianness.
+
+Options for reverse mode:
+  -f <format> output format, provided formats include squeezed/separated/decimal/oct.
+              (squeezed: 616263)
+              (separated: 0x61 0x62 0x63)
+              (decimal: 97 98 99)
+              (oct: 141 142 143)
+              You can also specify a custom format, see hexdump manual.
+
+Examples:
+  # Convert hexadecimal string to binary format.
+  hd p -s 616263
+  echo -n 616263 | hd p
+
+  # Convert string to hexadecimal string format.
+  hd p -s abc -r
+
+  # Convert string input through pipe to hexadecimal string format.
+  echo -n abc | hd p -r
+
+  # Convert a text file to hexadecimal string format.
+  hd p -r foo.txt
+
+  # Convert a binary file to hexadecimal string format.
+  hd p -r bar.bin
 EOF
   exit 1
 }
 
 cmd_parse() {
-  local reverse_mode hex_str infile
+  local reverse_mode input_str infile
   local reverse_continuous_bytes_order
+  local format="squeezed"
   while [ "$#" -gt 0 ]; do
     case "$1" in
       -s)
         shift
-        hex_str="$1"
+        input_str="$1"
         ;;
       -r)
         reverse_mode=1
         ;;
-      --reverse-continuous--bytes-order)
+      --reverse-continuous-bytes-order)
         reverse_continuous_bytes_order=1
+        ;;
+      -f)
+        shift
+        format="$1"
         ;;
       -*)
         usage_parse
@@ -41,32 +75,63 @@ cmd_parse() {
   done
 
   if [ "$reverse_mode" = "1" ]; then
-    if [ -z "$infile" ]; then
-      abort "A binary file is required."
+    case "$format" in
+      squeezed)
+        format='16/1 "%02x"'
+        ;;
+      separated)
+        format='"0x" 1/1 "%02x" " "'
+        ;;
+      decimal)
+        format='"" 1/1 "%d" " "'
+        ;;
+      oct)
+        format='"" 1/1 "%o" " "'
+        ;;
+    esac
+
+    if [ ! -t 0 ]; then
+      hexdump -v -e "$format"
+    else
+      if [ -n "$input_str" ]; then
+        echo -n "$input_str" | hexdump -v -e "$format"
+      else
+        if [ -z "$infile" ]; then
+          abort "A file is required."
+        fi
+        if [ ! -e "$infile" ]; then
+          abort "File $infile does not exist."
+        fi
+        hexdump -v -e "$format" "$infile"
+      fi
     fi
-    if [ ! -e "$infile" ]; then
-      abort "File $infile does not exist."
-    fi
-    hexdump -e '16/1 "%02x"' "$infile"
     echo
   else
-    if [ -n "$hex_str" ]; then
+    if [ ! -t 0 ]; then
       if [ "$reverse_continuous_bytes_order" = "1" ]; then
-        echo "$hex_str" | go run "$hexdump_dir/reverse_continuous_bytes_order.go"
+        go run "$hexdump_dir/reverse_continuous_bytes_order.go"
       else
-        echo "$hex_str" | xxd -r -p
+        xxd -r -p
       fi
     else
-      if [ -z "$infile" ]; then
-        abort "An input file is required or a hexadecimal string should be specified by '-s'."
-      fi
-      if [ ! -e "$infile" ]; then
-        abort "File $infile does not exist."
-      fi
-      if [ "$reverse_continuous_bytes_order" = "1" ]; then
-        go run "$hexdump_dir/reverse_continuous_bytes_order.go" "$infile"
+      if [ -n "$input_str" ]; then
+        if [ "$reverse_continuous_bytes_order" = "1" ]; then
+          echo "$input_str" | go run "$hexdump_dir/reverse_continuous_bytes_order.go"
+        else
+          echo "$input_str" | xxd -r -p
+        fi
       else
-        xxd -r -p "$infile"
+        if [ -z "$infile" ]; then
+          abort "An input file is required or a hexadecimal string should be specified by '-s'."
+        fi
+        if [ ! -e "$infile" ]; then
+          abort "File $infile does not exist."
+        fi
+        if [ "$reverse_continuous_bytes_order" = "1" ]; then
+          go run "$hexdump_dir/reverse_continuous_bytes_order.go" "$infile"
+        else
+          xxd -r -p "$infile"
+        fi
       fi
     fi
   fi
