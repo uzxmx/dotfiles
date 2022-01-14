137947a151724bec62365e7c59ba4d7462ef702f
Mingxiang Xue <mingxiangxue@gmail.com>
2022-01-14 12:11:57 +0800

    Add shell function to parse host from HTTP url
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/lib/url.sh
+++ scripts/lib/url.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-# This function parses the path part from a given url.
+# This function parses the host from a given HTTP url.
 #
 # @params:
-#   $1: the url
+#   $1: the HTTP url
+#   $2: whether to remove the port
+url_get_host() {
+  local url="$1"
+  local remove_port="$2"
+
+  url="$(echo "$url" | sed -E "s/^https?:\/\///")"
+  url="$(echo "$url" | sed -E "s/^([^\/]+).*$/\1/")"
+
+  if [ "$remove_port" = "1" ]; then
+    echo "$url" | sed "s/:.*$//"
+  else
+    echo "$url"
+  fi
+}
+
+# This function parses the path from a given HTTP url.
+#
+# @params:
+#   $1: the HTTP url
 #   $2: whether to remove the prefixed slash
 url_get_path() {
   local url="$1"
