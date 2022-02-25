0ae9d565e031e9c5eacb5b41e23f9246372d440f
Mingxiang Xue <mingxiangxue@gmail.com>
2022-02-25 19:04:20 +0800

    Add extra fzf options
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/lib/ps.sh
+++ scripts/lib/ps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ps_fzf_select_pid() {
-  ps -A -o pid,command | sed 1d | fzf --with-nth=2.. | awk '{print $1}'
+  ps -A -o pid,command | sed 1d | fzf "$@" --with-nth=2.. | awk '{print $1}'
 }
