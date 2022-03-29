25addb5cfbc93851c114f4056b25b69486515563
Mingxiang Xue <mingxiangxue@gmail.com>
2022-03-29 21:05:41 +0800

    Add installation script for GNU binutils for Mac OSX
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/lib/install.sh
+++ scripts/lib/install.sh
@@ -3,6 +3,7 @@
 # This script is a composite script which provides useful utilities for
 # installing software on different systems.
 
+. $(dirname "$BASH_SOURCE")/tmpfile.sh
 . $(dirname "$BASH_SOURCE")/utils.sh
 . $(dirname "$BASH_SOURCE")/git.sh
 . $(dirname "$BASH_SOURCE")/dmg.sh
