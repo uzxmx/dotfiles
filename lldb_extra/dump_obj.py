a47a657d9992a1c0efa85985145a30995556e706
Mingxiang Xue <mingxiangxue@gmail.com>
2022-04-15 21:33:09 +0800

    Remove lldb
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- lldb_extra/dump_obj.py
+++
@@ -1,9 +0,0 @@
-import lldb
-from lldb_extra import utils
-
-def dump_obj(obj_pointer, no_ivars = False):
-    '''The `obj_pointer` can be a memory address, or an expression which
-    returns a pointer to an object.
-    '''
-    opts = { 'obj_pointer': obj_pointer, 'no_ivars': 1 if no_ivars else 0 }
-    return utils.render_script_and_evaluate('dump_obj.mm', opts)
--- scripts/lldb/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-usage_entrypoint() {
-  "$dotfiles_dir/bin/otool" entrypoint -h
-}
-
-cmd_entrypoint() {
-  "$dotfiles_dir/bin/otool" entrypoint "$@"
-}
-alias_cmd e entrypoint
