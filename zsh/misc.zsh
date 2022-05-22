8ba8f85e1503aa21e4bd0053de360d1e86dd61f5
Mingxiang Xue <mingxiangxue@gmail.com>
2022-05-22 15:08:16 +0800

    Install fzf for non-intrusive mode
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,28 +1,4 @@
 # Hook that zsh will invoke when a command cannot be found.
 command_not_found_handler() {
-  local name="$1"
-  echo "Oops, command '$name' not found."
-
-  ask() {
-    echo -n "Would you like me to install it for you? (Y/n)"
-    local input
-    read input
-    if ! [ -z "$input" -o "$input" = "Y" -o "$input" = "y" ]; then
-      echo Cancelled.
-      exit
-    fi
-  }
-
-  case "$name" in
-    dig | whois | tcpdump | route)
-      ask
-      "$DOTFILES_DIR/scripts/install/network_tools"
-      ;;
-    *)
-      echo "Sorry, I couldn't help to install it, please do it by yourself."
-      echo "Please also consider whether to update 'command_not_found_handler' so I could do it for you later."
-      exit 1
-      ;;
-  esac
-  echo 'Installed successfully.'
+  "$DOTFILES_DIR/scripts/misc/command_not_found_handler" "$@"
 }
