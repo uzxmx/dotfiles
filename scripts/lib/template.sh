6bf969a606c7acb60c8e8afa21adb0f446e8cfc3
Mingxiang Xue <mingxiangxue@gmail.com>
2022-06-17 11:16:13 +0800

    Add examples for rendering shell template file
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/lib/template.sh
+++ scripts/lib/template.sh
@@ -4,6 +4,34 @@
 #
 # @params:
 #   $1: shell template file
+#
+# Note: characters like " | $ ` must be escaped.
+#
+# @example:
+#   # Render a json template file.
+#   cat <<EOF >foo.tpl.sh
+#   {
+#     \"codeLens.subseparator\": \"|\"
+#   }
+#   EOF
+#
+#   # Render a yaml template file.
+#   ACCESS_KEY="foo"
+#   WILDCARD=1
+#   DOMAIN="example.com"
+#   cat <<-EOF >bar.tpl.sh
+#   data:
+#     key: \"$(echo $ACCESS_KEY | base64)\"
+#   dnsNames:
+#     - $DOMAIN
+#   $(sh -c "
+#     if [ -n '$WILDCARD' ]; then
+#     echo '\
+#     - *.$DOMAIN
+#   '
+#     fi
+#   ")
+#   EOF
 render_shell_template_file() {
   eval "echo -e \"$(cat "$1")\""
 }
