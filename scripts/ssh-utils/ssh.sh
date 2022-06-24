2976e70a10ead17b3d12796182462e2718f09738
Mingxiang Xue <mingxiangxue@gmail.com>
2022-06-24 19:52:15 +0800

    Enable http proxy by default
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/ssh-utils/ssh.sh
+++ scripts/ssh-utils/ssh.sh
@@ -8,23 +8,26 @@ build the ssh command and connect to the remote.
 All arguments after '-' should be valid ssh arguments, and will be passed into
 ssh intactly.
 
+By default an HTTP proxy will be listening at 8123 port on the remote. You can
+disable this behavior by specifying '--disable-http-proxy'.
+
 Options:
   -d, --dry-run Only output SSH command
-  -p Enable http proxy
+  --disable-http-proxy
 EOF
   exit 1
 }
 
 cmd_ssh() {
-  local host_label dry_run
+  local host_label dry_run disable_http_proxy
   local -a opts
   while [ $# -gt 0 ]; do
     case "$1" in
       -d)
         dry_run=1
         ;;
-      -p)
-        opts+=(-R 8123:localhost:8123)
+      --disable-http-proxy)
+        disable_http_proxy=1
         ;;
       -)
         shift
@@ -44,11 +47,14 @@ cmd_ssh() {
     shift
   done
 
+  if [ -z "$disable_http_proxy" ]; then
+    opts+=(-R 8123:localhost:8123)
+  fi
+
   if [ -z "$host_label" ]; then
     host_label=$("$DOTFILES_DIR/scripts/bin/select-ssh-host")
   fi
   [ -z "$host_label" ] && exit
-
 
   local cmd=$("$DOTFILES_DIR/scripts/bin/build-ssh-cmd" "$host_label")
   if [ -z "$dry_run" ]; then
