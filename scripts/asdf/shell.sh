bc26e3dd15d5719605958c15e12d5400164e3ffa
Mingxiang Xue <mingxiangxue@gmail.com>
2022-09-20 15:50:20 +0800

    Update asdf wrapper utility
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/asdf/shell.sh
+++ scripts/asdf/shell.sh
@@ -5,14 +5,19 @@ cmd_shell() {
 
   local plugin current version prompt
   plugin="$(select_plugin --prompt "Change version in current shell> ")"
-  current="$(asdf current "$plugin" 2>/dev/null | awk '{print $1}')"
+  current="$(asdf current "$plugin" 2>/dev/null | awk '{print $2}' || true)"
   if [ -z "$current" ]; then
     prompt="No current version"
   else
     prompt="Current version: $current"
   fi
-  version="$(asdf list "$plugin" | fzf --prompt "$prompt> " | "$dotfiles_dir/bin/trim")"
-  echo asdf shell "$plugin" "$version" ";" echo "$plugin" version has been changed to "$version" in current shell. >&3
-  exit 104
+  version="$(asdf list "$plugin" | fzf --prompt "$prompt> " | "$DOTFILES_DIR/bin/trim" | sed 's/^\*//')"
+  if { true >&3; } 2> /dev/null; then
+    echo asdf shell "$plugin" "$version" ";" echo "$plugin" version has been changed to "$version" in current shell. >&3
+    exit 104
+  else
+    echo -e "Run below command in your shell:\n"
+    echo asdf shell "$plugin" "$version"
+  fi
 }
 alias_cmd s shell
