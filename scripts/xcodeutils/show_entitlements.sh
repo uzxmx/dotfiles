6e8542a6bdc57dd441081e3673e26994e228a8cd
Mingxiang Xue <mingxiangxue@gmail.com>
2022-10-21 16:58:56 +0800

    Update subcommand
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/xcodeutils/show_entitlements.sh
+++ scripts/xcodeutils/show_entitlements.sh
@@ -1,9 +1,9 @@
 usage_show_entitlements() {
   cat <<-EOF
-Usage: xcodeutils show_entitlements <app-path>
+Usage: xcodeutils show_entitlements <app-path> | <executable-file>
 
-Show entitlements of an IPA file. You need to unzip the IPA file first, and
-then pass in the path to the app in Payload.
+Show entitlements of an executable or IPA file. For IPA file, you need to unzip
+the IPA file first, and then pass in the path to the app in Payload.
 
 Example:
   $> xcodeutils show_entitlements Payload/foo.app
@@ -12,10 +12,5 @@ EOF
 }
 
 cmd_show_entitlements() {
-  if [ -z "$1" -o ! -d "$1" ]; then
-    echo 'You should pass in a valid path.'
-    exit 1
-  fi
-
   codesign -d --entitlements :- "$1"
 }
