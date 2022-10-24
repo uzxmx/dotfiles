235cc70c4bb68c7fc7d094f7b8982cc642bc3edf
Mingxiang Xue <mingxiangxue@gmail.com>
2022-10-24 16:23:19 +0800

    Add unix timestamp related functions
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/lib/date.sh
+++ scripts/lib/date.sh
@@ -4,3 +4,20 @@
 convert_en_US_to_unix_time() {
   LANG=en_US.UTF-8 date -j -f "%a %b %d %T %Z %Y" "$1" "+%s"
 }
+
+# %A: day of week
+convert_unix_timestamp_to_human_readable() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    date --date "@$1" +"+%Y-%m-%d %H:%M:%S"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    date -r "$1" "+%Y-%m-%d %H:%M:%S"
+  fi
+}
+
+get_unix_timestamp_ms() {
+  if type -p gdate &>/dev/null; then
+    gdate +%s%3N
+  else
+    date +%s%3N
+  fi
+}
