1d846586456ecf876e1b101bd269b13b96c51909
Mingxiang Xue <mingxiangxue@gmail.com>
2023-04-19 15:47:01 +0800

    Add compress subcommand for pdf
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

---
+++ scripts/pdf/compress.sh
@@ -0,0 +1,19 @@
+usage_compress() {
+  cat <<-EOF
+Usage: pdf compress <input-pdf-file> <output-pdf-file>
+
+Compress pdf to a readable quality by ghostscript.
+EOF
+  exit 1
+}
+
+cmd_compress() {
+  local infile="$1"
+  local outfile="$2"
+
+  [ -z "$infile" ] && abort "An input pdf file is required."
+  [ -z "$outfile" ] && abort "An output pdf file is required."
+
+  gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$outfile" "$infile"
+}
+alias_cmd c compress
