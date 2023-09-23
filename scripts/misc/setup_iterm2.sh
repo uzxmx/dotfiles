693f4f71386809cc402c0294973766e0dac4b4ff
Mingxiang Xue <mingxiangxue@gmail.com>
2023-09-23 13:23:29 +0800

    Fix home dir
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/misc/setup_iterm2.sh
+++ scripts/misc/setup_iterm2.sh
@@ -5,7 +5,7 @@
 set -e
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.dotfiles/iterm2"
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
