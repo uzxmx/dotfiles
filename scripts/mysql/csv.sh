c0ea2561a328c93fbc2f6b8a410dc1b2c6b80bcc
Mingxiang Xue <mingxiangxue@gmail.com>
2024-01-03 12:15:44 +0800

    Add csv2xls
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/mysql/csv.sh
+++ scripts/mysql/csv.sh
@@ -5,4 +5,8 @@ usage_csv() {
 cmd_csv() {
   source "$DOTFILES_DIR/scripts/lib/go.sh"
   go_run_compiled "$mysql_dir/csv.go" "$@"
+
+  echo "If you want to convert csv to excel, run below command:"
+  echo
+  echo "csv2xls --csv-delimiter , --csv-file-name file.csv --xls-file-name file.xls"
 }
