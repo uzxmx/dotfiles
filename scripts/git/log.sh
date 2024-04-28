af32d46bf9ec550735ed5fc23b7f7c65af7c4de8
Mingxiang Xue <mingxiangxue@gmail.com>
2024-04-28 10:17:25 +0800

    Able to show a git file history
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/git/log.sh
+++ scripts/git/log.sh
@@ -6,10 +6,14 @@ Enhanced log utility by fzf.
 
 Options:
   -r, --range use range mode. This will ask you to select a start and end commit.
+  -f Show a file history. This will ask you to select a file by fzf.
 
 Commit format:
   commit1..commit2: This will show you commits that can be reached from
                     commit2, but not from commit1. (a.k.a between)
+
+Examples:
+  $> g l -f
 EOF
   exit 1
 }
@@ -65,12 +69,38 @@ select_commit() {
   fi
 }
 
+show_history_for_file() {
+  local file="$1"
+  local default_prompt="F1:newest-commit CTRL-Y:yank-commit CTRL-V:show-commit"
+
+  local preview_window
+  if [ "$(tput cols)" -ge 160 ]; then
+    preview_window="right"
+  else
+    preview_window="bottom"
+  fi
+
+  local selection="$(git log --first-parent --pretty="format:%h %<(30,trunc)%s %ai %an %d" -- "$file" \
+    | fzf --no-mouse --cycle \
+    --layout=reverse \
+    --prompt="${default_prompt}>" \
+    --preview="git show {1} --first-parent --pretty='' --color=always -- \"$file\" | less -r" \
+    --preview-window="$preview_window:50%:wrap" \
+    --bind=f1:top \
+    --bind "ctrl-y:execute-silent(echo -n {1} | trim | cb && tmux display-message yanked)" \
+    --bind "ctrl-v:execute-silent(tmux display-popup -d '#{pane_current_path}' -T \" \$(git log {1} -1 --oneline) \" -w 90% -h 90% -E \"tmux new-session 'tmux set status off && $DOTFILES_DIR/bin/g show_commit {1}'\")"
+  )"
+}
+
 cmd_l() {
-  local range_mode commit
+  local range_mode commit show_file_history
   while [ "$#" -gt 0 ]; do
     case "$1" in
       -r | --range)
         range_mode=1
+        ;;
+      -f)
+        show_file_history=1
         ;;
       -*)
         usage_l
@@ -90,6 +120,11 @@ cmd_l() {
     fi
     if [ -n "$start_commit" -a -n "$end_commit" ]; then
       select_commit "$start_commit~1..$end_commit"
+    fi
+  elif [ "$show_file_history" = "1" ]; then
+    local file=$(fe --print)
+    if [ -n "$file" ]; then
+      show_history_for_file "$file"
     fi
   else
     local commit="$(select_commit --select $commit)"
