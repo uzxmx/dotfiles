b7d19b20dc9832b29ceb4be442c296101ee2133d
Mingxiang Xue <mingxiangxue@gmail.com>
2024-05-22 09:41:26 +0800

    Update v2ray config command
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/v2ray/config.sh
+++ scripts/v2ray/config.sh
@@ -1,5 +1,57 @@
+usage_config() {
+  cat <<-EOF
+Usage: v2ray config [config-type]
+
+The predefined config type includes 'socks_server'.
+If no config type is specified, it will open an editor for you to edit.
+
+Options:
+  -f Force to overwrite the config file if it exists
+
+Example:
+  $> v2ray config socks_server -f
+EOF
+  exit 1
+}
+
 cmd_config() {
+  local force config_type
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -f)
+        force=1
+        ;;
+      -*)
+        usage_config
+        ;;
+      *)
+        if [ -n "$config_type" ]; then
+          abort "Only on config type can be specified"
+        fi
+        config_type="$1"
+        ;;
+    esac
+    shift
+  done
+
   local configfile="/usr/local/etc/v2ray/config.json"
+
+  if [ -n "$config_type" ]; then
+    local template
+    case "$config_type" in
+      socks_server)
+        template="server/socks_config.json.sample"
+        ;;
+      *)
+        abort "Unsupported config type '$config_type'"
+        ;;
+    esac
+    [ -f "$configfile" ] && [ -z "$force" ] && abort "A config file already exists. You may specify '-f' to overwrite."
+    sudo mkdir -p "$(dirname "$configfile")"
+    sudo cp "$DOTFILES_DIR/v2ray/$template" "$configfile"
+    "$DOTFILES_DIR/bin/v2ray" restart
+  fi
+
   local old_md5
   if [ ! -e "$configfile" ]; then
     local template="$(find "$DOTFILES_DIR/v2ray" -name '*.json.sample' | fzf --prompt 'Select a template file to copy from: ')"
