8b49de872546327ad54712b702350b65116e58df
Mingxiang Xue <mingxiangxue@gmail.com>
2025-12-27 21:34:19 +0800

    Add example
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/pdf/from_images.sh
+++ scripts/pdf/from_images.sh
@@ -3,6 +3,9 @@ usage_from_images() {
 Usage: pdf from_images <image-file>... <pdf-file>
 
 Convert images to a pdf. By default, the size of the generated pdf is A4.
+
+Examples:
+  $> ls | sort -h | xargs -J % pdf from_images % output.pdf
 EOF
   exit 1
 }
