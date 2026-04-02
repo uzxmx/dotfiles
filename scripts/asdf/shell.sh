f57d8ead2747b4630b0caaaf486c9c9ab69dab78
Mingxiang Xue <mingxiangxue@gmail.com>
2026-04-02 16:13:14 +0800

    Update asdf shell subcommand
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/asdf/shell.sh
+++ scripts/asdf/shell.sh
@@ -1,7 +1,23 @@
 source "$(dirname $BASH_SOURCE)/common.sh"
 
+_asdf_shell_export() {
+  local plugin="$1" version="$2"
+  local env_var="ASDF_$(echo "$plugin" | tr '[:lower:]-' '[:upper:]_')_VERSION"
+  local export_cmd="export ${env_var}=${version}"
+  if { true >&3; } 2>/dev/null; then
+    echo "$export_cmd" >&3
+    exit 104
+  else
+    echo -e "Run below command in your shell:\n"
+    echo "$export_cmd"
+  fi
+}
+
 cmd_shell() {
-  [ "$#" -gt 0 ] && (asdf shell "$@" || true) && exit
+  if [ "$#" -gt 1 ]; then
+    _asdf_shell_export "$1" "$2"
+    return
+  fi
 
   local plugin current version prompt
   plugin="$(select_plugin --prompt "Change version in current shell> ")"
@@ -12,12 +28,6 @@ cmd_shell() {
     prompt="Current version: $current"
   fi
   version="$(asdf list "$plugin" | fzf --prompt "$prompt> " | "$DOTFILES_DIR/bin/trim" | sed 's/^\*//')"
-  if { true >&3; } 2> /dev/null; then
-    echo asdf shell "$plugin" "$version" ";" echo "$plugin" version has been changed to "$version" in current shell. >&3
-    exit 104
-  else
-    echo -e "Run below command in your shell:\n"
-    echo asdf shell "$plugin" "$version"
-  fi
+  _asdf_shell_export "$plugin" "$version"
 }
 alias_cmd s shell
