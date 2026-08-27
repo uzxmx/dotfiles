d8ad19c8acc466ab59fa84ac36eade9f9e5317bd
Mingxiang Xue <mingxiangxue@gmail.com>
2026-08-27 23:41:04 +0800

    Add tuna mirror
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- scripts/pip/mirror.sh
+++ scripts/pip/mirror.sh
@@ -1,23 +1,37 @@
 usage_mirror() {
   cat <<-EOF
-Usage: pip mirror [-e | -d]
+Usage: pip mirror [-e [aliyun|tuna]] [-d]
 
 Manage pip mirror. By default it shows whether mirror is
 enabled.
 
 Options:
-  -e Enable mirror
-  -d Disable mirror
+  -e [name] Enable mirror (default: aliyun)
+              aliyun - Aliyun mirror
+              tuna   - Tsinghua TUNA mirror
+  -d        Disable mirror
 EOF
   exit 1
 }
 
 cmd_mirror() {
   local action="show"
+  local mirror="aliyun"
   while [ "$#" -gt 0 ]; do
     case "$1" in
       -e)
         action="enable"
+        case "$2" in
+          aliyun | ali | tuna | tsinghua)
+            mirror="$2"
+            shift
+            ;;
+          -*)
+            ;;
+          ?*)
+            usage_mirror
+            ;;
+        esac
         ;;
       -d)
         action="disable"
@@ -30,13 +44,22 @@ cmd_mirror() {
   done
 
   if [ "$action" = "enable" ]; then
-    pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+    local url
+    case "$mirror" in
+      aliyun | ali)
+        url="https://mirrors.aliyun.com/pypi/simple/"
+        ;;
+      tuna | tsinghua)
+        url="https://pypi.tuna.tsinghua.edu.cn/simple"
+        ;;
+    esac
+    pip config set global.index-url "$url"
   elif [ "$action" = "disable" ]; then
-    pip config unset global.index-url
+    pip config unset global.index-url 2>/dev/null || true
   fi
 
   local url
-  url=$(pip config list 2>/dev/null | grep '^global.index-url=' | cut -d= -f2-)
+  url=$(pip config list 2>/dev/null | grep '^global.index-url=' | cut -d= -f2- || true)
   if [ -n "$url" ]; then
     echo "mirror: $url"
   else
